0c4855b437fa0e3a8acc68fc6be39051060ebcf6
andrewblond <andrewblond@yandex.com>
2014-06-01 19:31:14 +0400

    Removed `block-keeps` plugin

--- plugins/keeps.js
+++ plugins/keeps.js
@@ -1,12 +1,19 @@
+var path = require('path');
 var builder = require('./builders/keeps');
-var buildKeeps = require('./lib/keep-build');
+var bemdeclByKeeps = require('./techs/bemdecl-by-keeps');
 
 module.exports = function (maker) {
     return {
         build: function (options) {
+            var pattern = path.join(options.destPath, '*');
             var resolve = builder(options);
 
-            return buildKeeps(maker, resolve, options);
+            maker._config.nodes(pattern, function (nodeConfig) {
+                nodeConfig.addTech(bemdeclByKeeps);
+                nodeConfig.addTarget('?.bemdecl.js');
+            });
+
+            return maker.build(resolve, options);
         }
     };
 };
