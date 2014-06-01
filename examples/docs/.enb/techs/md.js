a19fa0ed645417b2ffd310ca83d56ac9fe425ed0
andrewblond <andrewblond@yandex.com>
2014-06-01 19:31:26 +0400

    Removed `docs` example

--- examples/docs/.enb/techs/md.js
+++
@@ -1,6 +0,0 @@
-module.exports = require('enb/lib/build-flow').create()
-    .name('md')
-    .target('target', '?.md')
-    .useFileList('md')
-    .justJoinFiles()
-    .createTech();
--- examples/docs/blocks/block/__elem/block__elem.md
+++
@@ -1,2 +0,0 @@
-The elem of block
-=================
