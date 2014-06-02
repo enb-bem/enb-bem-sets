ffa95f4843f2d38746d14770727839cf6303d5dd
andrewblond <andrewblond@yandex.com>
2014-06-03 02:29:40 +0400

    Removed `bundles` plugin

--- plugins/bundles.js
+++
@@ -1,9 +0,0 @@
-var builder = require('./builders/bundles');
-
-module.exports = function (maker) {
-    return {
-        build: function (options) {
-            return maker.build(builder(options), options);
-        }
-    };
-};
