46e2569908985be2422e548af1ddb8199cbc86e8
andrewblond <andrewblond@yandex.com>
2014-07-30 23:55:01 +0400

    Merged `server` & `sets` bin

--- bin/sets.js
+++ bin/sets.js
@@ -2,13 +2,16 @@
 var META_TASK_NAME = '__sets__';
 var path = require('path');
 var coa = require('coa');
+var vow = require('vow');
 var pkg = require('../package.json');
-var version = pkg.version;
 var MakePlatform = require('enb/lib/make');
+var Server = require('../lib/server/server');
 var makePlatform = new MakePlatform();
+var cdir = process.cwd();
 
 coa.Cmd()
     .name('sets')
+    .title('BEM sets platform')
     .helpful()
     .opt()
         .name('version')
@@ -17,76 +20,110 @@ coa.Cmd()
         .long('version')
         .flag()
         .act(function () {
-            return version;
+            return pkg.version;
         })
         .end()
-    .opt()
-        .name('noCache')
-        .title('Drop cache before running make')
-        .short('n')
-        .long('no-cache')
-        .flag()
+    .cmd()
+        .name('make')
+        .title('build specified targets')
+        .helpful()
+        .opt()
+            .name('noCache')
+            .title('drop cache before running make')
+            .short('n')
+            .long('no-cache')
+            .flag()
+            .end()
+        .opt()
+            .name('dir')
+            .title('custom project root')
+            .short('d')
+            .long('dir')
+            .def(process.cwd())
+            .end()
+        .opt()
+            .name('hideWarnings')
+            .title('hides warnings')
+            .long('hide-warnings')
+            .flag()
+            .end()
+        .opt()
+            .name('graph')
+            .title('draws build graph')
+            .long('graph')
+            .flag()
+            .end()
+        .arg()
+            .name('targets')
+            .title('path targets to build')
+            .req()
+            .arr()
+            .end()
+        .act(function (opts, args) {
+            return makePlatform.init(path.resolve(opts.dir))
+                .then(function () {
+                    if (opts.hideWarnings) {
+                        makePlatform.getLogger().hideWarnings();
+                    }
+
+                    if (!opts.noCache) {
+                        makePlatform.loadCache();
+                    }
+
+                    return makePlatform.buildTask(META_TASK_NAME, args.targets)
+                        .then(function () {
+                            if (opts.graph) {
+                                console.log(makePlatform.getBuildGraph().render());
+                            }
+
+                            makePlatform.saveCache();
+                            return makePlatform.destruct();
+                        });
+                })
+                .fail(function (err) {
+                    if (opts.graph) {
+                        console.log(makePlatform.getBuildGraph().render());
+                    }
+
+                    console.error(err.stack);
+                    process.exit(1);
+                });
+        })
         .end()
-    .opt()
-        .name('dir')
-        .title('Custom project root')
-        .short('d')
-        .long('dir')
-        .def(process.cwd())
+    .cmd()
+        .name('server')
+        .title('run development server')
+        .helpful()
+        .opt()
+            .name('port')
+            .title('socket port [8080]')
+            .short('p')
+            .long('port')
+            .def(8080)
+            .end()
+        .opt()
+            .name('host')
+            .title('socket host [0.0.0.0]')
+            .long('host')
+            .def('0.0.0.0')
+            .end()
+        .opt()
+            .name('socket')
+            .title('unix socket path')
+            .short('s')
+            .long('host')
+            .end()
+        .act(function (opts) {
+            var server = new Server();
+
+            vow.when(server.init(cdir, opts))
+                .then(function () {
+                    return server.run();
+                })
+                .fail(function (err) {
+                    console.error(err.stack);
+                    process.exit(1);
+                });
+        })
         .end()
-    .opt()
-        .name('hideWarnings')
-        .title('Hides warnings')
-        .long('hide-warnings')
-        .flag()
-        .end()
-    .opt()
-        .name('graph')
-        .title('Draws build graph')
-        .long('graph')
-        .flag()
-        .end()
-    .arg()
-        .name('args')
-        .title('Task or path/to/target')
-        .arr()
-        .end()
-    .act(function (opts, args) {
-        args = args.args || [];
-
-        return makePlatform.init(path.resolve(opts.dir))
-            .then(function () {
-                var taskNames = Object.keys(makePlatform._projectConfig._tasks);
-
-                if (opts.hideWarnings) {
-                    makePlatform.getLogger().hideWarnings();
-                }
-
-                if (!opts.noCache) {
-                    makePlatform.loadCache();
-                }
-
-                if (!args.length || args.length && taskNames.indexOf(args[0]) === -1) {
-                    args.unshift(META_TASK_NAME);
-                }
-
-                return makePlatform.build(args)
-                    .then(function () {
-                        if (opts.graph) {
-                            console.log(makePlatform.getBuildGraph().render());
-                        }
-
-                        makePlatform.saveCache();
-                        return makePlatform.destruct();
-                    });
-            })
-            .fail(function (err) {
-                if (opts.graph) {
-                    console.log(makePlatform.getBuildGraph().render());
-                }
-
-                console.error(err.stack);
-                process.exit(1);
-            });
-    })
     .run(process.argv.slice(2));
